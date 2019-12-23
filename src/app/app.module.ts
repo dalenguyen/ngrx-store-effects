00ce7822edf7d5fec4ab490bdc982da96ab3fb4e
Dale Nguyen <dungnq@itbox4vn.com>
2019-12-23 14:31:32 -0500

    Aadded Router State Serializers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,54 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { Routes, RouterModule } from '@angular/router'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { StoreModule, MetaReducer } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import { StoreModule, MetaReducer } from '@ngrx/store'
+import { EffectsModule } from '@ngrx/effects'
+import { reducers, CustomSerializer } from './store'
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer
+} from '@ngrx/router-store'
 
 // not used in production
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { storeFreeze } from 'ngrx-store-freeze';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { storeFreeze } from 'ngrx-store-freeze'
 
 // this would be done dynamically with webpack for builds
 const environment = {
   development: true,
-  production: false,
-};
+  production: false
+}
 
 export const metaReducers: MetaReducer<any>[] = !environment.production
   ? [storeFreeze]
-  : [];
+  : []
 
 // bootstrap
-import { AppComponent } from './containers/app/app.component';
+import { AppComponent } from './containers/app/app.component'
 
 // routes
 export const ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   {
     path: 'products',
-    loadChildren: '../products/products.module#ProductsModule',
-  },
-];
+    loadChildren: '../products/products.module#ProductsModule'
+  }
+]
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(ROUTES),
-    StoreModule.forRoot({}, { metaReducers }),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreRouterConnectingModule,
     EffectsModule.forRoot([]),
-    environment.development ? StoreDevtoolsModule.instrument() : [],
+    environment.development ? StoreDevtoolsModule.instrument() : []
   ],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   declarations: [AppComponent],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
